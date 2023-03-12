7a308252f6a68ce17ec982e8ad892c0cccde3fd2
Muhammed Ramzy <57300921+Mohamedramzii@users.noreply.github.com>
2023-03-12 22:54:58 +0200

    Login View completely done

--- lib/features/auth/presentation/views/loginview.dart
+++ lib/features/auth/presentation/views/loginview.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_conditional_rendering/flutter_conditional_rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:xstore_cubit/features/auth/presentation/view_model/cubits/LOGIN_CUBIT/login_cubit.dart';
+import 'package:xstore_cubit/features/auth/presentation/views/widgets/custom_Toast_Widget.dart';
 import 'package:xstore_cubit/features/auth/presentation/views/widgets/custom_button_widget.dart';
 import 'package:xstore_cubit/features/auth/presentation/views/widgets/custom_haveaccount_widget.dart';
 import 'package:xstore_cubit/features/auth/presentation/views/widgets/custom_textformfield_widget.dart';
@@ -7,94 +11,144 @@ import 'package:xstore_cubit/features/auth/presentation/views/widgets/custom_tex
 class LoginView extends StatelessWidget {
   LoginView({super.key});
 
-  final GlobalKey _formKey = GlobalKey<FormFieldState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController passwordcontroller = TextEditingController();
   TextEditingController emailcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20.w,
-            // vertical: 90.h,
-          ),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('LOGIN', style: Theme.of(context).textTheme.displayLarge),
-                  SizedBox(
-                    height: 10.h,
+    return BlocProvider(
+        create: (context) => LoginCubit(),
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state is LoginSuccessState) {
+              if (state.loginModel.status!) {
+                CustomToastWidget.getToast(
+                    text: state.loginModel.message!, color: Colors.green);
+              } else {
+                CustomToastWidget.getToast(
+                    text: state.loginModel.message!, color: Colors.red);
+              }
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              // appBar: AppBar(),
+              body: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.w,
+                    // vertical: 90.h,
                   ),
-                  Text('Login now and enjoy with our offers!',
-                      style: Theme.of(context).textTheme.displayMedium),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  CustomTextFormFieldWidget(
-                    controller: emailcontroller,
-                    keyboardtype: TextInputType.emailAddress,
-                    labeltext: 'Email',
-                    validate: (String? newValue) {
-                      if (newValue!.isEmpty) {
-                        return 'Please enter a valid email address';
-                      }
-                      return null;
-                    },
-                    onsave: (String? newValue) {
-                      emailcontroller.text = newValue!;
-                    },
-                    isPassword: false,
-                    prefixicon: const Icon(Icons.email),
-                  ),
-                  SizedBox(
-                    height: 15.h,
-                  ),
-                  CustomTextFormFieldWidget(
-                    controller: passwordcontroller,
-                    keyboardtype: TextInputType.text,
-                    labeltext: 'Password',
-                    validate: (String? newValue) {
-                      if (newValue!.isEmpty) {
-                        return 'Password must be at least 8 characters';
-                      }
-                      return null;
-                    },
-                    onsave: (String? newValue) {
-                      passwordcontroller.text = newValue!;
-                    },
-                    isPassword: true,
-                    prefixicon: const Icon(Icons.lock),
-                    sufixicon: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.visibility),
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('LOGIN',
+                              style: Theme.of(context).textTheme.displayLarge),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text('Login now and enjoy with our offers!',
+                              style: Theme.of(context).textTheme.displayMedium),
+                          SizedBox(
+                            height: 20.h,
+                          ),
+                          CustomTextFormFieldWidget(
+                            controller: emailcontroller,
+                            keyboardtype: TextInputType.emailAddress,
+                            labeltext: 'Email',
+                            validate: (String? newValue) {
+                              if (newValue!.isEmpty) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                            onsave: (String? newValue) {
+                              emailcontroller.text = newValue!;
+                            },
+                            isPassword: false,
+                            prefixicon: const Icon(Icons.email),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                            height: 15.h,
+                          ),
+                          CustomTextFormFieldWidget(
+                            controller: passwordcontroller,
+                            keyboardtype: TextInputType.text,
+                            labeltext: 'Password',
+                            validate: (String? newValue) {
+                              if (newValue!.isEmpty) {
+                                return 'Password must be at least 8 characters';
+                              }
+                              return null;
+                            },
+                            onsave: (String? newValue) {
+                              passwordcontroller.text = newValue!;
+                            },
+                            isPassword: LoginCubit.get(context).isHidden,
+                            prefixicon: const Icon(Icons.lock),
+                            sufixicon: IconButton(
+                              onPressed: () {
+                                LoginCubit.get(context).visibilityChange();
+                              },
+                              icon: Icon(LoginCubit.get(context).isHidden
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
+                            textInputAction: TextInputAction.done,
+                            onsubmitted: (value) {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+                                LoginCubit.get(context).loginUser(
+                                    email: emailcontroller.text.trim(),
+                                    password: passwordcontroller.text.trim());
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            height: 20.h,
+                          ),
+                          Conditional.single(
+                            context: context,
+                            conditionBuilder: (context) =>
+                                state is! LoginLoadingState,
+                            widgetBuilder: (context) => CustomButtonWidget(
+                              text: 'LOGIN',
+                              onpressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  LoginCubit.get(context).loginUser(
+                                      email: emailcontroller.text.trim(),
+                                      password: passwordcontroller.text.trim());
+                                }
+                              },
+                            ),
+                            fallbackBuilder: (context) => const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          CustomhaveAccountWidget(
+                            text: 'Don\'t have an account?',
+                            toText: 'REGISTER',
+                            onpressed: () {},
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  const CustomButtonWidget(text: 'LOGIN'),
-                  SizedBox(
-                    height: 3.h,
-                  ),
-                  CustomhaveAccountWidget(
-                    text: 'Don\'t have an account?',
-                    toText: 'REGISTER',
-                    onpressed: () {},
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
-        ),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
